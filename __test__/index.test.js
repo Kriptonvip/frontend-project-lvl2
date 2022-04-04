ea6a3e5d98e80bbef73ff8104ba492b44350b230
Kripton <simenk@mail.ru>
2022-04-04 19:12:46 +0300

    test corrections

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -5,25 +5,25 @@ import plain from '../src/formatters/plain.js';
 test('check rigth work json', () => {
   const filepath1 = './__fixtures__/file1.json';
   const filepath2 = './__fixtures__/file2.json';
-  const correctAnswer = fs.readFileSync('./__fixtures__/result.txt', 'utf-8').trim();
+  const correctAnswer = fs.readFileSync('./__fixtures__/result_stylish.txt', 'utf-8').trim();
   expect(compare(filepath1, filepath2)).toBe(correctAnswer);
 });
 test('check rigth work YAML', () => {
   const filepath1 = './__fixtures__/file1.yml';
   const filepath2 = './__fixtures__/file2.yml';
-  const correctAnswer = fs.readFileSync('./__fixtures__/result.txt', 'utf-8').trim();
+  const correctAnswer = fs.readFileSync('./__fixtures__/result_stylish.txt', 'utf-8').trim();
   expect(compare(filepath1, filepath2)).toBe(correctAnswer);
 });
 test('check plain text format yml', () => {
   const filepath1 = './__fixtures__/file1.yml';
   const filepath2 = './__fixtures__/file2.yml';
-  const correctAnswer = fs.readFileSync('./__fixtures__/plain.txt', 'utf-8').trim();
+  const correctAnswer = fs.readFileSync('./__fixtures__/result_plain.txt', 'utf-8').trim();
   expect(compare(filepath1, filepath2, plain)).toBe(correctAnswer);
 });
 test('check plain text format json', () => {
   const filepath1 = './__fixtures__/file1.json';
   const filepath2 = './__fixtures__/file2.json';
-  const correctAnswer = fs.readFileSync('./__fixtures__/plain.txt', 'utf-8').trim();
+  const correctAnswer = fs.readFileSync('./__fixtures__/result_plain.txt', 'utf-8').trim();
   expect(compare(filepath1, filepath2, plain)).toBe(correctAnswer);
 });
 test('check JSON text format json', () => {
