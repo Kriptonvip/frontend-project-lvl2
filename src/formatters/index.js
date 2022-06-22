fbd902c600afe2dcfd31394e308db6ad48e8ea77
Kripton <simenk@mail.ru>
2022-06-22 21:56:51 +0300

    upgrade AST and refactoring add export genDiff

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,14 +2,11 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 
 export default (data, formatName) => {
-  if (formatName === 'stylish') {
-    return stylish(data);
-  }
   if (formatName === 'plain') {
     return plain(data);
   }
   if (formatName === 'json') {
     return JSON.stringify(data);
   }
-  return new Error(`Unknown format: '${formatName}'!`);
+  return stylish(data);
 };
