43bc0a81ec6a83a117ef68c1519174259363265b
Kripton <simenk@mail.ru>
2022-06-28 15:59:21 +0300

    yml , yaml, json, without dot

--- src/getData.js
+++ src/getData.js
@@ -6,13 +6,10 @@ const getData = (fileName) => {
   const filePath = path.resolve(fileName);
   console.log(filePath, 1);
   const file = fs.readFileSync(filePath, 'utf8');
-  // объясните пожалуйста зачем строки 11 - 18 выносить в отдельный модуль?
-  // Получается надо сделать функцию определения формата, потом функцию парсер
-  // которая вернёт данные, или оставить как есть эти 7 строк, всё ради абстрагирования?
-  if (fileName.endsWith('.json')) {
+  if (fileName.endsWith('json')) {
     return JSON.parse(file);
   }
-  if (fileName.endsWith('.yaml') || fileName.endsWith('.yml')) {
+  if (fileName.endsWith('yaml') || fileName.endsWith('yml')) {
     return YAML.parse(file);
   }
   return Error('File path or extension is wrong');
