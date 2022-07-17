94dc75cd6dc5fc370ff4b511d13cf2361493e915
Kripton <kripton@iMac-Kripton.lan>
2022-07-17 18:47:37 +0300

    console.log delete

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ const genDiff = (filepath1, filepath2, formatName) => {
   const file2 = getData(filepath2);
   const data = compare(file1, file2);
   const result = diff(data, formatName);
-  console.log(result);
   return result;
 };
 
